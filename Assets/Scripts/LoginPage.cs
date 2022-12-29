46378ddc3dd51bca7ccbc38f1195d82034e19818
Ben Mazurek <wakanda3001@gmail.com>
2022-12-29 01:36:03 -0500

    changed to steam

--- Assets/Scripts/LoginPage.cs
+++ Assets/Scripts/LoginPage.cs
@@ -1,4 +1,5 @@
 using Mirror;
+using Steamworks;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -19,29 +20,69 @@ public class LoginPage : MonoBehaviour
     public GameObject loginPage;
     public GameObject chatPage;
 
+    //Steam shit
+    public NetworkManager networkManager;
+    protected Callback<LobbyCreated_t> lobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> lobbyRequested;
+    protected Callback<LobbyEnter_t> lobbyEntered;
+    private const string hostAdressKey = "HostAddress";
+
+
     private void Update()
     {
         playerColor = Color.HSVToRGB(slider.value, 1, 1);
         playerName = nameInput.text;
         text.color = playerColor;
-        NetworkManager.singleton.networkAddress = address.text;
     }
 
-    public void hostServer()
+    private void Start()
     {
-        NetworkManager.singleton.GetComponent<TelepathyTransport>().port = ushort.Parse(port.text);
-        NetworkManager.singleton.StartHost();
+        if (!SteamManager.Initialized) { return; }
+        lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        lobbyRequested = Callback<GameLobbyJoinRequested_t>.Create(OnLobbyRequested);
+        lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+
+    }
+    public void HostSteamLobby()
+    {
+        SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, networkManager.maxConnections);
+    }
+
+    private void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        if(callback.m_eResult != EResult.k_EResultOK)
+        {
+            return;
+        }
+
+        networkManager.StartHost();
+        SteamMatchmaking.SetLobbyData(
+            new CSteamID(callback.m_ulSteamIDLobby),
+            hostAdressKey,
+            SteamUser.GetSteamID().ToString());
+
         loginPage.SetActive(false);
         chatPage.SetActive(true);
-
     }
 
-    public void joinServer()
+    private void OnLobbyRequested(GameLobbyJoinRequested_t callback)
     {
-        NetworkManager.singleton.networkAddress = address.text;
-        NetworkManager.singleton.GetComponent<TelepathyTransport>().port = ushort.Parse(port.text);
-        NetworkManager.singleton.StartClient();
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
+
+    private void OnLobbyEntered(LobbyEnter_t callback)
+    {
+        if(NetworkServer.active) { return; }
+
+        string hostAddress = SteamMatchmaking.GetLobbyData(
+            new CSteamID(callback.m_ulSteamIDLobby),
+            hostAdressKey);
+
+        networkManager.networkAddress= hostAddress;
+        networkManager.StartClient();
+
         loginPage.SetActive(false);
         chatPage.SetActive(true);
     }
 }
+
